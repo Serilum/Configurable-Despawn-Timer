fa8b655a28e33906c7f55b869ccf17f498bbefb1
Rick South <rick@natamus.com>
2024-07-28 07:39:40 +0200

    (v4.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/configurabledespawntimer/ModForge.java
+++ Forge/src/main/java/com/natamus/configurabledespawntimer/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.configurabledespawntimer;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.configurabledespawntimer.forge.config.IntegrateForgeConfig;
 import com.natamus.configurabledespawntimer.forge.events.ForgeDespawnEvents;
 import com.natamus.configurabledespawntimer.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
