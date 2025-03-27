b3d06b6c15f2d94e0cd0ef9c42a34c9c2b99542c
Rick South <rick@natamus.com>
2025-03-28 00:57:16 +0100

    Configurable Despawn Timer source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/configurabledespawntimer/forge/events/ForgeDespawnEvents.java
@@ -0,0 +1,27 @@
+package com.natamus.configurabledespawntimer.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.configurabledespawntimer.cmd.CommandCdt;
+import com.natamus.configurabledespawntimer.events.DespawnEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeDespawnEvents {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		DespawnEvents.onWorldLoad((ServerLevel)level);
+	}
+
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	CommandCdt.register(e.getDispatcher());
+    }
+}
