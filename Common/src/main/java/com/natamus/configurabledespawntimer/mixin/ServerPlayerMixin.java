e2f0fecbef472574c8c00d4464933b0865b25482
Rick South <rick@natamus.com>
2026-04-10 02:59:56 +0200

    Configurable Despawn Timer source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/configurabledespawntimer/mixin/ServerPlayerMixin.java
@@ -0,0 +1,21 @@
+package com.natamus.configurabledespawntimer.mixin;
+
+import com.natamus.configurabledespawntimer.util.Reference;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(value = ServerPlayer.class, priority = 1001)
+public abstract class ServerPlayerMixin {
+	@Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
+	public void drop(ItemStack itemStack, boolean bl, boolean bl2, CallbackInfoReturnable<ItemEntity> cir, ItemEntity itemEntity) {
+		if (itemEntity != null) {
+			itemEntity.addTag(Reference.MOD_ID + ".player_drop");
+		}
+	}
+}
