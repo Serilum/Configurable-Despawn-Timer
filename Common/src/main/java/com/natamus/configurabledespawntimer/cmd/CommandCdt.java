16bccf6b5f6a3b7db3d1bd7a7f9fe4444cb98721
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:37:36 +0200

    (v4.5) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/configurabledespawntimer/cmd/CommandCdt.java
+++ Common/src/main/java/com/natamus/configurabledespawntimer/cmd/CommandCdt.java
@@ -1,4 +1,5 @@
 package com.natamus.configurabledespawntimer.cmd;
+import com.natamus.configurabledespawntimer.util.Reference;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.natamus.collective.functions.MessageFunctions;
@@ -31,22 +32,22 @@ public class CommandCdt {
 				try {
 					Util.loadItemConfig(source.getLevel());
 				} catch (Exception ex) {
-					MessageFunctions.sendMessage(source, "Something went wrong while reloading the despawn timer config file.", ChatFormatting.RED);
+					MessageFunctions.sendTranslatableMessage(source, "collective.configurabledespawntimer.message.somethingwentwrong", ChatFormatting.RED);
 					ex.printStackTrace();
 					return 0;
 				}
 				
-				MessageFunctions.sendMessage(source, "Successfully loaded the despawn timer config file.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.configurabledespawntimer.message.successfullyloadeddespawn", ChatFormatting.DARK_GREEN);
 				return 1;
 			}))
 		);
 	}
 	
 	private static void showUsage(CommandSourceStack source) {
-		MessageFunctions.sendMessage(source, "Configurable Despawn Timer Usage:", ChatFormatting.DARK_GREEN, true);
+		MessageFunctions.sendTranslatableMessage(source, "collective.shared.message.usage", true, ChatFormatting.DARK_GREEN, Reference.NAME);
 		MessageFunctions.sendMessage(source, " /cdt usage", ChatFormatting.DARK_GREEN);
-		MessageFunctions.sendMessage(source, "  Show this message.", ChatFormatting.DARK_GRAY);
+		MessageFunctions.sendTranslatableMessage(source, "  ", "collective.shared.message.showmessage", ChatFormatting.DARK_GRAY);
 		MessageFunctions.sendMessage(source, " /cdt reload", ChatFormatting.DARK_GREEN);
-		MessageFunctions.sendMessage(source, "  Reloads the config file.", ChatFormatting.DARK_GRAY);
+		MessageFunctions.sendTranslatableMessage(source, "  ", "collective.shared.message.reloadsconfigfile", ChatFormatting.DARK_GRAY);
 	}
 }
